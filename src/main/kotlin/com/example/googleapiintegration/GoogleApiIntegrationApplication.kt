7bb36cad4d34828e3ba957c79a23612a023eda40
Iman Soltani <iman.soltani.j@gmail.com>
2023-02-22 12:09:09 +0330

    Initialize Springboot 3.0.2

---
+++ src/main/kotlin/com/example/googleapiintegration/GoogleApiIntegrationApplication.kt
@@ -0,0 +1,11 @@
+package com.example.googleapiintegration
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoogleApiIntegrationApplication
+
+fun main(args: Array<String>) {
+	runApplication<GoogleApiIntegrationApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/googleapiintegration/GoogleApiIntegrationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.googleapiintegration
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GoogleApiIntegrationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
